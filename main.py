d640974ae4f2e6971f130e648ae7f720065b17ae
Dulavinya <dulavinyamanawadu@gmail.com>
2025-04-10 20:43:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import cv2
+from init import get_limits
+
+
+yellow = (0, 255, 255) #yellow in BGR colourspace
 
 cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
 
+    hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lowerLimit, upperLimit =get_limits(color=yellow)
+
+
+
+    mask =cv2.inRange(hsvImage,lowerLimit, upperLimit)
     cv2.imshow('frame',frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
