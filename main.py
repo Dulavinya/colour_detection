475a8197f91ace80c366a1beaa0867402e469fc5
Dulavinya <dulavinyamanawadu@gmail.com>
2025-04-10 21:05:34 +0530

    added the box

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+from PIL import Image
 from init import get_limits
 
 
@@ -15,6 +16,17 @@ while True:
 
 
     mask =cv2.inRange(hsvImage,lowerLimit, upperLimit)
+    mask_ = Image.fromarray(mask)
+
+    bbox = mask_.getbbox()
+
+    if bbox is not None:
+        x1, y1, x2, y2 = bbox
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.putText(frame, "yellow", (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+       
+
+    print(bbox)
     cv2.imshow('frame',frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
